a65bee7b67f95dad04e3ec061dc2e80de0fcf4e9
Cristiano do Nascimento <cristiano.n@gmail.com>
2022-07-06 14:54:21 -0300

    feat: #14 - Barra de ferramentas da listagem

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,18 @@
+import { BarraDeFerramentas } from '../../shared/components';
+import { LayoutBaseDePagina } from '../../shared/layouts';
+
+export const Dashboard = () => {
+  return (
+    <LayoutBaseDePagina
+      titulo='Página inicial'
+      barraDeFerramentas={(
+        <BarraDeFerramentas
+          mostrarInputBusca
+          textoBotaoNovo='Nova'
+        />
+      )}
+    >
+      Testando
+    </LayoutBaseDePagina>
+  );
+};
