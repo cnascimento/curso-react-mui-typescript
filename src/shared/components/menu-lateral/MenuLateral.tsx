c2ea6287418a629e96edf817dc668ee3492ef332
Cristiano do Nascimento <cristiano.n@gmail.com>
2022-09-09 15:02:15 -0300

    #bonus Troca da imagem do avatar

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -45,7 +45,7 @@ export const MenuLateral: React.FC = ({ children }) => {
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
             <Avatar
               sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
-              src="https://media-exp1.licdn.com/dms/image/C4E03AQHqnjAspOtqlg/profile-displayphoto-shrink_800_800/0/1568220924187?e=1662595200&v=beta&t=65clPL8D6TncEL5UodZ7cqrS8fdeMPBQuXfO93qW05s"
+              src="/avatar.jpg"
             />
           </Box>
 
